0df7d07227c213b336095b47c4c77005d6542320
IanlEhn <ianlehschool@gmail.com>
2021-11-29 04:00:04 +0000

    add five circles to project

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -24,19 +24,19 @@ var init = function (window) {
         var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-       function drawCircle() {
+       var drawCircle = function(){
         circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-        physikz.addRandomVelocity(circle, canvas);
+        physikz.addRandomVelocity(circle, canvas, 10, 10);
         view.addChild(circle);
         circles.push(circle);
-       };
-    
+       }
 
         // TODO 3 / 8 : Call the drawCircle() function 
-      
-       for (var i = 0; i < 5; i++){
-           drawCircle();
-       }
+      drawCircle();
+      drawCircle();
+      drawCircle();
+      drawCircle();
+      drawCircle();
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -49,10 +49,20 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+           
+        
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
 
             // TODO 9 : Iterate over the array
            
@@ -92,8 +102,8 @@ var init = function (window) {
         
         app.addUpdateable(window.opspark.game);
     }
-};
 
+};
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
